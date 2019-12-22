b71927bcdbfb278d2ef5b08ab3b09090aacf715d
Damian Ruiz <nrt.damian@gmail.com>
2019-12-21 23:27:48 -0500

    File updated

--- config/orm.js
+++ config/orm.js
@@ -1,3 +1,26 @@
+// const connection = require("./connection");
 
-// Here is the O.R.M. where you write functions that takes inputs and conditions
-// and turns them into database commands like SQL.
+// const orm = {
+//     selectNonDevoured: function(data) {    
+//             connection.query(`SELECT id, name from burger where devoured = false`), function (err, result) {
+//                 if (err) throw err;
+//                 console.log(result);
+//                 return data(result);
+//             }
+//     },
+
+//     selectDevoured: function (data) {
+//         connection.query(`SELECT id, name from burger where devoured = true`), function (err, result) {
+//             if (err) throw err;
+//             data(result);
+//         }
+//     },
+
+//     updateOne: function (id) {
+//         connection.query(`UPDATE burger set devoured = true where id = ${id}`, function (err, result) {
+//             if (err) throw err;
+//         })
+//     }
+// }
+
+// module.exports = orm;
