87529f3e77fb730603320c73046e719d846e6176
Damian Ruiz <nrt.damian@gmail.com>
2019-12-21 23:29:04 -0500

    JS script created

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,1 +1,33 @@
-// jquery code goes here
+const submit = document.getElementById("submit");
+const burgerInput = document.getElementById("burgerInput");
+const devour = document.querySelectorAll(".devour");
+
+
+//Creates new http object
+const http = new XMLHttpRequest();
+
+//Function to send POST requests;
+
+//Send request to create new burger record in DB.
+submit.addEventListener("click", function (event) {
+    let burger = burgerInput.value;
+    console.log(burger);
+    http.open("POST", '/api/newburger', true);
+    http.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    http.send(`burger=${burger}`);
+    event.preventDefault();
+    location.reload();
+})
+
+//Send request to change "devoured" status of a single burger
+devour.forEach((button) => {
+button.addEventListener("click", function(event) {
+    let id = this.getAttribute("data-id");
+    console.log(id);
+    http.open("POST", `/api/devoured/${id}`, true);
+    http.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    event.preventDefault();
+    http.send();
+    location.reload();
+});
+})
