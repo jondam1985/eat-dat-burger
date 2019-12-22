6e1a3dcd1fee89414ea32392caf851f334bb7110
Damian Ruiz <nrt.damian@gmail.com>
2019-12-21 23:28:10 -0500

    Database schema

--- db/schema.sql
+++ db/schema.sql
@@ -1,1 +1,10 @@
--- create database and table schema here
+CREATE DATABASE burgers_db;
+
+USE burgers_db;
+
+CREATE TABLE burger (
+id int not null auto_increment,
+name varchar(255) not null,
+devoured boolean not null default false,
+primary key(id)
+);
