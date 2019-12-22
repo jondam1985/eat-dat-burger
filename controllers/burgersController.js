bd649ec41f0bdc8c94fc6aa87f0772ed9dd7a29d
Damian Ruiz <nrt.damian@gmail.com>
2019-12-21 23:27:56 -0500

    File updated.

--- controllers/burgersController.js
+++ controllers/burgersController.js
@@ -1,1 +1,17 @@
-// placeholder for controller code
+const express = require("express");
+const burger = require("../models/burger");
+const orm = require("../config/orm");
+
+const route = express.Router();
+
+//Listens to GET requests
+route.get("/", function (req, res) {
+    burger.nonDevoured(function(data) {
+        var burgerObj = {
+            devoured: data
+        }
+        res.render("index", burgerObj);
+    });
+})
+
+module.exports = route;
